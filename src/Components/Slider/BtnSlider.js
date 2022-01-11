d231259bcaecddc7b01ef6062b7571ff1a8c533b
Pinchon Léo <leopinchonspa@gmail.com>
2022-01-11 19:02:16 +0100

    récupération des fichiers afin de push

---
+++ src/Components/Slider/BtnSlider.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import leftArrow from './icons/left-arrow.svg'
+import rightArrow from './icons/right-arrow.svg'
+
+export default function BtnSlider(props) {
+    console.log(props);
+    return (
+        <button 
+        onClick = {props.moveSlide}
+        className={props.direction === "next" ? "btn-slide next" : "btn-slide prev"}>
+            <img src={props.direction === "next" ? rightArrow : leftArrow} alt="" />
+        </button>
+    )
+}
